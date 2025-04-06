0cef172f887530ac01f958650b6363831fcca644
Hunter Macy <huntermacy@Hunters-MacBook-Pro.local>
2025-04-06 14:48:15 -0600

    front-end updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, Box } from '@chakra-ui/react';
-import { HelmetProvider } from 'react-helmet-async';
-import { UserProvider } from './context/UserContext';
-import { ProgressProvider } from './context/ProgressContext';
-import ScrollToTop from './components/ScrollToTop';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import CourseDetail from './pages/CourseDetail';
-import ModuleView from './pages/ModuleView';
-import NotFound from './pages/NotFound';
+import { Box, Container, Heading, Text, Button, VStack } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
-const App: React.FC = () => {
+function App() {
+  const navigate = useNavigate()
+
   return (
-    <HelmetProvider>
-      <ChakraProvider>
-        <UserProvider>
-          <ProgressProvider>
-            <Router>
-              <ScrollToTop />
-              <Box minH="100vh" display="flex" flexDirection="column">
-                <Header />
-                <Box flex="1" bg="gray.50">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/courses" element={<Dashboard />} />
-                    <Route path="/course/:courseId" element={<CourseDetail />} />
-                    <Route
-                      path="/course/:courseId/module/:moduleId"
-                      element={<ModuleView />}
-                    />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </Box>
-                <Footer />
-              </Box>
-            </Router>
-          </ProgressProvider>
-        </UserProvider>
-      </ChakraProvider>
-    </HelmetProvider>
-  );
-};
+    <Box minH="100vh" bg="gray.50">
+      <Container maxW="container.xl" py={20}>
+        <VStack spacing={8} textAlign="center">
+          <Heading
+            as="h1"
+            size="2xl"
+            bgGradient="linear(to-r, blue.400, purple.600)"
+            bgClip="text"
+          >
+            First Generation Investors
+          </Heading>
+          <Text fontSize="xl" color="gray.600" maxW="2xl">
+            Empowering first-generation investors with the tools and knowledge they need to succeed in the financial markets.
+          </Text>
+          <Button
+            size="lg"
+            colorScheme="blue"
+            onClick={() => navigate('/dashboard')}
+          >
+            Get Started
+          </Button>
+        </VStack>
+      </Container>
+    </Box>
+  )
+}
 
-export default App;
+export default App 
